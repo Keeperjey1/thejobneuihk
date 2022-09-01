ae69fd88ae58d21404f79346161e6a7255835f75
Keeperjey1 <keeperjey1@yahoo.de>
2022-09-01 14:00:54 +0200

    onItemMehrfach

--- app/src/main/java/de/jey/thejobneu_v1/MyProfile.java
+++ app/src/main/java/de/jey/thejobneu_v1/MyProfile.java
@@ -23,11 +23,23 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class MyProfile extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    Spinner spinBranche, spinJobs;
+    Spinner spinBranche;
+    Spinner spinJobs;
+    Spinner spinJobs2;
+    Spinner spinJobs3;
+    Spinner spinErfBra;
+    private View view;
+    private int i;
+    private long l;
+    Spinner spiErfJob;
+    Spinner spiErfJob2;
+    Spinner spiErfJob3;
     ArrayList<String> brancheList = new ArrayList<>();
     ArrayList<String> jobList = new ArrayList<>();
+    ArrayList<String> erfList = new ArrayList<>();
     ArrayAdapter<String> brancheAdapter;
     ArrayAdapter<String> jobsAdapter;
+    ArrayAdapter<String> erfAdapter;
     RequestQueue requestQueue;
   /*  String[] branche = {"Gastronomie", "Medizin", "Informatik"};
     String[] jobs = {"Barman", "Kellner", "Thekenkraft", "Betriebsleiter"};
@@ -38,9 +50,16 @@ public class MyProfile extends AppCompatActivity implements AdapterView.OnItemSe
         setContentView(R.layout.activity_my_profile);
         spinBranche = findViewById(R.id.spiBranche);
         spinJobs = findViewById(R.id.spiJob);
+        spinJobs2 = findViewById(R.id.spiJob2);
+        spinJobs3 = findViewById(R.id.spiJob3);
+        spinErfBra = findViewById(R.id.spiBraErf);
+        spiErfJob = findViewById(R.id.spiJobErf);
+        spiErfJob2 = findViewById(R.id.spiJobErf2);
+        spiErfJob3 = findViewById(R.id.spiJobErf3);
+
         requestQueue = Volley.newRequestQueue(this);
         String brancheUrl = "http://192.168.64.150/buero2/dropdownbranche.php";
-       // String jobsUrl = "http://192.168.64.150/buero/dropdownjobs.php";
+        //String jobsUrl = "http://192.168.64.150/buero/dropdownjobs.php";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
                 brancheUrl, null, new Response.Listener<JSONObject>() {
             @Override
@@ -69,14 +88,52 @@ public class MyProfile extends AppCompatActivity implements AdapterView.OnItemSe
         requestQueue.add(jsonObjectRequest);
         spinBranche.setOnItemSelectedListener(this);
 
+
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        if (adapterView.getId() == R.id.spiBranche) {
+    public void onItemSelected(AdapterView<?> spinBranche, View view, int i, long l ) {
+        if (spinBranche.getId() == R.id.spiBranche) {
+            erfList.clear();
+            String selectedBranche = spinBranche.getSelectedItem().toString();
+            String erfUrl = "http://192.168.64.150/buero2/dropdownerf.php?branchenname="+selectedBranche;
+            requestQueue = Volley.newRequestQueue(this);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                    erfUrl, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("erfahrung");
+                        for (int i = 0; i<jsonArray.length(); i++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            String bezeichnung = jsonObject.optString("bezeichnung");
+                            erfList.add(bezeichnung);
+                            erfAdapter = new ArrayAdapter<>(MyProfile.this, R.layout.item_file, erfList);
+                            erfAdapter.setDropDownViewResource(R.layout.item_file);
+                            spinErfBra.setAdapter(erfAdapter);
+                        }
+
+                    }catch (JSONException e) {
+                        e.printStackTrace();
+
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+            spinErfBra.setOnItemSelectedListener(this);
+        }
+
+    }
+  /*  public void onItemSelected2(AdapterView<?> spinErfBra, View view, int i, long l) {
+        if (spinErfBra.getId() == R.id.spiBraErf) {
             jobList.clear();
-            String selectedBranche = adapterView.getSelectedItem().toString();
-            String jobsUrl = "http://192.168.64.150/buero2/dropdownjobs.php?branchenname="+selectedBranche;
+            String selectedErf = spinErfBra.getSelectedItem().toString();
+            String jobsUrl = "http://192.168.64.150/buero2/dropdownjobs.php?bezeichnung="+selectedErf;
             requestQueue = Volley.newRequestQueue(this);
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
                     jobsUrl, null, new Response.Listener<JSONObject>() {
@@ -86,8 +143,8 @@ public class MyProfile extends AppCompatActivity implements AdapterView.OnItemSe
                         JSONArray jsonArray = response.getJSONArray("jobs");
                         for (int i = 0; i<jsonArray.length(); i++) {
                             JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            String branchenname = jsonObject.optString("job_name");
-                            jobList.add(branchenname);
+                            String bezeichnung = jsonObject.optString("job_name");
+                            jobList.add(bezeichnung);
                             jobsAdapter = new ArrayAdapter<>(MyProfile.this, R.layout.item_file, jobList);
                             jobsAdapter.setDropDownViewResource(R.layout.item_file);
                             spinJobs.setAdapter(jobsAdapter);
@@ -108,7 +165,7 @@ public class MyProfile extends AppCompatActivity implements AdapterView.OnItemSe
             requestQueue.add(jsonObjectRequest);
         }
 
-    }
+    }*/
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
