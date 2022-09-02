013ef104c755f1102083a90e879b33af7cd9fcfa
Keeperjey1 <keeperjey1@yahoo.de>
2022-09-02 09:19:48 +0200

    alles

--- app/src/main/java/de/jey/thejobneu_v1/MainActivity.java
+++ app/src/main/java/de/jey/thejobneu_v1/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         dialog.show();
 
         //receive data from php file
-        final StringRequest request = new StringRequest("http://192.168.0.105/buero/fetchbuero.php",new Response.Listener<String>() {
+        final StringRequest request = new StringRequest("http://192.168.64.150/buero2/fetchbuero2.php",new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 dialog.dismiss();
@@ -142,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
     public void startActivity(Intent intent) {
         super.startActivity(intent);
     }
-h
+
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.itLogin:
